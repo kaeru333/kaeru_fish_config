45bc0d9f3148bfcdfe2923b7472bb4ff52814aad
kaeru333 <yoshi264171@gmail.com>
2024-08-19 23:53:32 +0900

    bobthefish_setting

---
+++ myconf.d/bobthefish.fish
@@ -0,0 +1,12 @@
+set -g theme_nerd_fonts yes
+set -g theme_color_scheme dark
+set -g default_user bobthecow
+set -g theme_powerline_fonts yes
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt "\$ "
+set -g theme_display_user no
+set -g theme_display_sudo_user no
+set -g theme_display_hostname no
+set -g theme_display_virtualenv yes
+set -g theme_display_node yes
+set -g theme_display_git yes
