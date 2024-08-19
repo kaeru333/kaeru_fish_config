9e785b2e4c726b439c7b2906324de81b9afa66aa
kaeru333 <yoshi264171@gmail.com>
2024-08-19 23:17:14 +0900

    first_commit

---
+++ abbr/abbr.fish
@@ -0,0 +1,7 @@
+abbr -a vi nvim
+abbr -a :q exit
+abbr -a ls eza --icons --color
+abbr -a la eza -a
+abbr -a tree eza -T
+abbr -a open xdg-open
+abbr -a python python3
---
+++ conf.d/bobthefish.fish
@@ -0,0 +1,7 @@
+# set -g theme_nerd_fonts yes
+# set -g theme_color_scheme dark
+# set -g theme_display_user ssh
+# set -g default_user bobthecow
+# set -g theme_show_exit_status yes
+# set -g default_user your_normal_user
+# set -g theme_newline_cursor yes
