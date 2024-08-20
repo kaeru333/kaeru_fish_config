9f642d1d996f3fce71b4b9a90317cc89a59b2153
kaeru333 <yoshi264171@gmail.com>
2024-08-20 22:31:56 +0900

    atcoder-cli

--- abbr/abbr.fish
+++ abbr/abbr.fish
@@ -5,3 +5,5 @@ abbr -a la eza -a
 abbr -a tree eza -T
 abbr -a open xdg-open
 abbr -a python python3
+abbr -a pyt oj t -c '"python3 main.py" -d ./tests/'
+abbr -a submit acc s main.py -- --guess-python-interpreter pypy
--- myconf.d/bobthefish.fish
+++ myconf.d/bobthefish.fish
@@ -10,3 +10,5 @@ set -g theme_display_hostname no
 set -g theme_display_virtualenv yes
 set -g theme_display_node yes
 set -g theme_display_git yes
+set -g theme_display_date no
+set -g theme_title_display_user no
