5a8e51bc1470a7434fb57e749a40b8b5a1abcb32
kaeru333 <yoshi264171@gmail.com>
2024-09-23 12:43:36 +0900

    abbr_xdg-open_op

--- abbr/abbr.fish
+++ abbr/abbr.fish
@@ -3,7 +3,7 @@ abbr -a :q exit
 abbr -a ls eza --icons --color
 abbr -a la eza -a
 abbr -a tree eza -T
-abbr -a open xdg-open
+abbr -a op xdg-open
 abbr -a python python3
 abbr -a pyt oj t -c '"python3 main.py" -d ./tests/'
 # abbr -a submit acc s main.py -- --guess-python-interpreter pypy
